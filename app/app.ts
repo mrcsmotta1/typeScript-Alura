3ba8be9b38f02bda72d958a1e937fc61a8bfe4f9
Marcos Motta <marcos.motta@jsl.com.br>
2023-03-07 20:40:45 -0300

    Finalizando modulo 2

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,7 @@
-import { Negociacao } from "./models/negociacao.ts"
+import { Negociacao } from "./models/negociacao.js"
 
 const negociacao = new Negociacao(new Date(), 10, 100);
 
-console.log('11 >',negociacao.quantidade);
 
-// negociacao.quantidade =  1000;
-console.log('22 >',negociacao.volume);
+
+
